2ab6f35a3aebdf760542ab21850b662675631a69
asm <vaskocuturilo@rambler.ru>
2024-04-07 19:28:16 +0200

    DV-00008: Fix entity and tests.

--- src/main/java/com/example/questionpull/entity/AbstractBaseEntity.java
+++ src/main/java/com/example/questionpull/entity/AbstractBaseEntity.java
@@ -13,7 +13,4 @@ public abstract class AbstractBaseEntity {
     @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
     protected Integer id;
-
-    protected AbstractBaseEntity() {
-    }
 }
