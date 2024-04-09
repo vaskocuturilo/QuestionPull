d5c56587b67d74503c651db2379b3feae58790fd
asm <vaskocuturilo@rambler.ru>
2024-04-09 19:45:11 +0200

    DV-000010: Add question difficulty level.

--- src/main/java/com/example/questionpull/service/TelegramBot.java
+++ src/main/java/com/example/questionpull/service/TelegramBot.java
@@ -9,10 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -23,6 +28,8 @@ public class TelegramBot extends TelegramLongPollingBot {
     private final QuestionPullRepository questionPullRepository;
 
     private final StorageUtils storageUtils;
+
+    private static final String NEXT_QUESTION = "NEXT_QUESTION";
 
     @Autowired
     public TelegramBot(BotProperties config, QuestionPullRepository questionPullRepository, StorageUtils storageUtils) {
@@ -55,11 +62,11 @@ public class TelegramBot extends TelegramLongPollingBot {
                 case "/help" -> helpCommand(chatId);
                 case "/question" -> {
                     var question = getQuestionFromPull();
-                    question.ifPresentOrElse(questionPullEntity -> sendMessage(
-                                    String.format(""" 
-                                                    Title: %s,
+                    question.ifPresentOrElse(questionPullEntity -> addButtonAndSendMessage(
+                                    String.format("""
+                                                     Title: %s,
                                                     Question: %s
-                                                    """, questionPullEntity.getTitle(),
+                                                     """, questionPullEntity.getTitle(),
                                             questionPullEntity.getBody()), chatId),
                             () -> {
                                 throw new IllegalStateException("Can't take any question");
@@ -67,11 +74,26 @@ public class TelegramBot extends TelegramLongPollingBot {
                 }
                 default -> commandNotFound(chatId);
             }
+        } else if (update.hasCallbackQuery()) {
+
+            String callbackData = update.getCallbackQuery().getData();
+            long chatId = update.getCallbackQuery().getMessage().getChatId();
+
+            if (callbackData.equals(NEXT_QUESTION)) {
+
+                var joke = getQuestionFromPull();
+
+                joke.ifPresent(questionPullEntity -> addButtonAndEditText(String.format("""
+                                 Title: %s,
+                                Question: %s
+                                 """, questionPullEntity.getTitle(),
+                        questionPullEntity.getBody()), chatId, update.getCallbackQuery().getMessage().getMessageId()));
+            }
         }
     }
 
     private Optional<QuestionPullEntity> getQuestionFromPull() {
-        return questionPullRepository.getRandomQuestion();
+        return questionPullRepository.getRandomQuestion("easy");
     }
 
     private void commandNotFound(long chatId) {
@@ -97,6 +119,54 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     private void send(SendMessage msg) {
+        try {
+            execute(msg);
+        } catch (TelegramApiException e) {
+            log.error(Arrays.toString(e.getStackTrace()));
+        }
+    }
+
+    private void addButtonAndSendMessage(String textToSend, long chatId) {
+
+        SendMessage message = new SendMessage();
+        message.setText(textToSend);
+        message.setChatId(chatId);
+
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+        List<InlineKeyboardButton> rowInline = new ArrayList<>();
+        var inlinekeyboardButton = new InlineKeyboardButton();
+        inlinekeyboardButton.setCallbackData(NEXT_QUESTION);
+        inlinekeyboardButton.setText("next question");
+        rowInline.add(inlinekeyboardButton);
+        rowsInline.add(rowInline);
+        markupInline.setKeyboard(rowsInline);
+        message.setReplyMarkup(markupInline);
+        send(message);
+    }
+
+    private void addButtonAndEditText(String joke, long chatId, Integer messageId) {
+
+        EditMessageText message = new EditMessageText();
+        message.setChatId(chatId);
+        message.setText(joke);
+        message.setMessageId(messageId);
+
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+        List<InlineKeyboardButton> rowInline = new ArrayList<>();
+        var inlinekeyboardButton = new InlineKeyboardButton();
+        inlinekeyboardButton.setCallbackData(NEXT_QUESTION);
+        inlinekeyboardButton.setText("next question");
+        rowInline.add(inlinekeyboardButton);
+        rowsInline.add(rowInline);
+        markupInline.setKeyboard(rowsInline);
+        message.setReplyMarkup(markupInline);
+
+        sendEditMessageText(message);
+    }
+
+    private void sendEditMessageText(EditMessageText msg) {
         try {
             execute(msg);
         } catch (TelegramApiException e) {
