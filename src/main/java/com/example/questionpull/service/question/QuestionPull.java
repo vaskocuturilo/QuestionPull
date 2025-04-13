f0c0ba6a5bfe1ee1152790c3dc4260659d8fa4ed
vaskocuturilo <vaskocuturilo@rammbler.ru>
2025-04-13 19:23:27 +0200

    Dv-00030: Add change level logic

--- src/main/java/com/example/questionpull/service/question/QuestionPull.java
+++ src/main/java/com/example/questionpull/service/question/QuestionPull.java
@@ -2,7 +2,6 @@ package com.example.questionpull.service.question;
 
 import com.example.questionpull.entity.QuestionPullEntity;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,6 +9,4 @@ public interface QuestionPull {
     Optional<QuestionPullEntity> getRandomQuestion(final String level);
 
     void setActiveForQuestion(final UUID uuid);
-
-    Optional<QuestionPullEntity> getRandomQuestionExcludingIds(String level, List<UUID> excludedIds);
 }
